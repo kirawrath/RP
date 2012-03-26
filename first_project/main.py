62f649f12317cb4ab0ca88bed0f388e4a2776b37
Kira <kira@OVERLORD.(none)>
2012-03-26 15:20:34 -0300

    I should process the points in a random order...

--- first_project/main.py
+++ first_project/main.py
@@ -2,6 +2,7 @@
 import sys
 import random
 CLASSES_SIZE = 2
+from nearest_neighbour import *
 
 class Dot:
 	def __init__(self, position):
@@ -29,77 +30,6 @@ def hamming(dots):
 		distances.append(dist)
 	return distances
 
-def nearest_neighbour(dots):
-	m=[]
-	for dot in dots:
-		m.append(dot.pos)
-	distances = []
-	for i in m:
-		dist = []
-		for j in m:
-			if i == j:
-				continue
-			# Euclidian distance
-			d = pow(sum(map(lambda a, b: pow(b-a, 2), i, j)), 0.5)
-			d = round(d, 3)
-
-			dist.append(d)
-			
-		distances.append(dist)
-	return distances
-
-def k_nearest_neighbour(dots, k):
-	m=[]
-	for dot in dots:
-		m.append(dot.pos)
-	distances = nearest_neighbour(dots)
-	for idx, line in enumerate(distances):
-		#s = sorted(line)
-		smallers = []
-		votes = []
-		for dummy in range(CLASSES_SIZE):
-			smallers.append(float('inf'))
-		for dummy in line:
-			votes.append((0,0)) #(class, dist)
-
-		for i, dist in enumerate(line):
-			if dist < smallers[-1]:
-				''' index is the dots index.
-					As the distances lines has
-					(len(dots)-1) entries, this
-					index mapping is necessary.
-				'''
-				index = 0
-				if i < idx:
-					index = i
-				else:
-					index = i + 1
-
-				for v in votes:
-					if v[1] == 0:
-						v = (dots[index].clas, dist)
-						break
-				else:
-					for v in votes:
-						if v[1] == smallers[-1]:
-							v = (dots[index].clas, dist)
-					else:
-						raise 'Oh noes!'
-				
-				smallers[-1] = dist
-				smallers.sort()
-		results = [0]*CLASSES_SIZE
-		for v in votes:
-			results[v[0]] += 1
-		# The index of the bigger is the chosen one
-		bigger=0
-		for index, r in enumerate(results):
-			if r > bigger:
-				bigger = r
-				dots[idx].clas = index
-
-
-	return distances
 
 def main():
 	if len(sys.argv) > 1:
@@ -125,9 +55,9 @@ def main():
 
 	for dot in dots:
 		if random.random()>0.5:
-			dot.clas = 0
-		else:
 			dot.clas = 1
+		else:
+			dot.clas = 0
 	
 	for dot in dots:
 		print dot,
