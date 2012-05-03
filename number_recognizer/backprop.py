cd74baadaa5fb6e71d1398d953e683baf4380722
Kira <kirawrath@gmail.com>
2012-05-02 23:41:33 -0300

    Done!

--- number_recognizer/backprop.py
+++ number_recognizer/backprop.py
@@ -1,23 +1,40 @@
-#!/usr/bin/env python
-
-from digits import DigitsDataSet #@UnresolvedImport
-from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised import BackpropTrainer
 
+class backproptrainer(BackpropTrainer):
+	def __init__(self, module, dataset=None, learningrate=0.01, lrdecay=1.0,
+				momentum=0., verbose=False, batchlearning=False,
+				weightdecay=0.):
+		BackpropTrainer.__init__(self, module, dataset, learningrate, lrdecay,
+				momentum, verbose, batchlearning, weightdecay)
+	
+	def testOnData(self, dataset=None, verbose=False):
+		"""Compute the MSE of the module performance on the given dataset.
 
-def testTraining():
-	hidden_layers = 2
-	d = DigitsDataSet()
-	n = buildNetwork(d.indim, hidden_layers, d.outdim, recurrent=True)
-	t = BackpropTrainer(n, dataset = d, learningrate = 0.01, momentum = 0.1, verbose = False)
+		If no dataset is supplied, the one passed upon Trainer initialization is
+		used."""
+		if dataset == None:
+			dataset = self.ds
+		dataset.reset()
+		if verbose:
+			print '\nTesting on data:'
+		errors = []
+		importances = []
+		ponderatedErrors = []
+		results=[]
+		for seq in dataset._provideSequences():
+			self.module.reset()
+			e, i, result = dataset._evaluateSequence(self.module.activate, seq, verbose)
+			results.append(result)
+			importances.append(i)
+			errors.append(e)
+			ponderatedErrors.append(e / i)
+		if verbose:
+			print 'All errors:', ponderatedErrors
+		assert sum(importances) > 0
+		avgErr = sum(errors) / sum(importances)
+		if verbose:
+			print 'Average error:', avgErr
+			print ('Max error:', max(ponderatedErrors), 'Median error:',
+				   sorted(ponderatedErrors)[len(errors) / 2])
+		return avgErr, results
 
-	print 'Training...'
-	for _ in range(10000):
-		t.train()
-
-	print 'Testing...'
-	t.testOnData(verbose= True)
-
-
-if __name__ == '__main__':
-	testTraining()
